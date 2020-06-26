ac83f29d3c5e7879d1136e36414b04739b263ada
Kasey Arnold <kaseyarnold15@gmail.com>
2020-06-26 01:30:55 +0000

    Automatically backed up by Learn

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -1,7 +1,7 @@
 class RidesController < ApplicationController
     def create
         ride = Ride.create(ride_params)
-        message = ride.take_ride 
+        message = ride.take_ride
         redirect_to user_path(ride.user), flash: { message: message }
     end
 
