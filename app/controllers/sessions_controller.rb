308406704bb91a7a932b7cdbe800315b39adb3c7
Kasey Arnold <kaseyarnold15@gmail.com>
2020-06-26 01:31:23 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
     end
 
     def destroy
-        session.delete :user_id 
+        session.delete :user_id
         redirect_to root_url
     end
 
