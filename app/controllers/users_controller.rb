63ef4df0ba6a5d1c91632261a819198176f9e87d
Kasey Arnold <kaseyarnold15@gmail.com>
2020-06-26 01:32:20 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,17 +3,17 @@ class UsersController < ApplicationController
     skip_before_action :verified_user, only: [:new, :create]
 
     def new
-        @user = User.new 
+        @user = User.new
     end
 
     def create
         @user = User.new(user_params)
 
         if @user.save
-            session[:user_id] = @user.id 
+            session[:user_id] = @user.id
             redirect_to user_path(@user)
         else
-            render new_user_path 
+            render new_user_path
         end
     end
 
